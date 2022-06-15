b28335e0293dab812069f31eeaf6dc1b11e80525
O'Neal Ombu <ombu.oneal@yahoo.com>
2022-06-15 16:26:17 +0100

    Add files via upload

--- modal/contact.php
+++ modal/contact.php
@@ -3,7 +3,7 @@
 //Fetching Values from URL
 $php_name 		= $_POST['ajax_name'];
 $php_email 		= $_POST['ajax_email'];
-$php_emailto	= $_POST['ajax_emailto'];
+$php_emailto	= $_POST['hello@onealombu.xyz'];
 $php_message 	= $_POST['ajax_message'];
 
 
